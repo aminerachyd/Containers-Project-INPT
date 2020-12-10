8224609769a331a5cd728a216d499433a6a79dcd
Amine Rachyd <aminerachyd99@gmail.com>
2020-12-10 22:22:21 +0100

    Still testing

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 module.exports = {
   // TODO uncomment this
   // host: "localhost",
-  host: "mysql-container-p5",
+  // host: "mysql-container-p5",
+  host: "mysql",
   user: "root",
   password: "root",
   database: "db",
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const db = require("./config/db");
+const mysql = require("mysql");
 
 const fileupload = require("express-fileupload");
 
@@ -13,5 +15,19 @@ app.use(fileupload());
 app.use("/partie3", require("./routes/partie3"));
 
 const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server started at port ${PORT} ...`);
 
-app.listen(PORT, () => console.log(`Server started at port ${PORT} ...`));
+  let con = mysql.createConnection(db);
+
+  con.connect((err) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log("DB connectée");
+      con.end((err) => {
+        if (err) throw err;
+      });
+    }
+  });
+});
