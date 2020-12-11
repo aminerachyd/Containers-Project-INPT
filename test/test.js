302987b60cac36cd7dca7bdf8e4a99aaed7f81d1
Amine Rachyd <aminerachyd99@gmail.com>
2020-12-11 15:52:27 +0100

    Test de la route POST 6

--- test/test.js
+++ test/test.js
@@ -21,7 +21,7 @@ describe("/POST Ajout d'un personnage", () => {
     chai
       .request(app)
       .post("/partie3")
-      .attach("photo", fs.readFileSync("./naruto-test.jpg"), "naruto-test.jpg")
+      .attach("photo", fs.readFileSync("naruto-test.jpg"), "naruto-test.jpg")
       .send(json)
       .end((err, res) => {
         if (err) throw err;
