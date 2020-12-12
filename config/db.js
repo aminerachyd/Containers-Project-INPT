13f839cb6ba8d8d1b7fa0f5d38772ddbd272afc4
Amine Rachyd <aminerachyd99@gmail.com>
2020-12-12 19:44:14 +0100

    Testing Heroku 3-6

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 module.exports = {
   // XXX uncomment this
   // host: "localhost",
-  host: "mysql-container-p5",
+  // host: "mysql-container-p5",
+  host: "wp433upk59nnhpoh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   user: "root",
   password: "root",
   database: "db",
