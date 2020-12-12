a8e2468d095aa16f0369b8041fae872e0a75c91c
Amine Rachyd <aminerachyd99@gmail.com>
2020-12-12 16:45:23 +0100

    Testing Heroku 2-0
    J'ai pu déployer l'image Nodejs, mais la lecture de la base de données
    n'est pas opérationelle

--- index.js
+++ index.js
@@ -20,38 +20,38 @@ app.listen(PORT, () => {
 
   let con = mysql.createConnection(db);
 
-  con.connect((err) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log("DB connectée");
-      con.query("CREATE DATABASE IF NOT EXISTS db", (err) => {
-        if (err) {
-          throw err;
-        } else {
-          con.query("USE db;", (err) => {
-            if (err) {
-              throw err;
-            } else {
-              con.query(
-                "CREATE TABLE IF NOT EXISTS personnage (id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,name varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,photo varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,characteristics text COLLATE utf8mb4_unicode_ci);",
-                (err) => {
-                  if (err) {
-                    throw err;
-                  } else {
-                    console.log("Base de données initialisée");
-                    con.end((err) => {
-                      if (err) throw err;
-                    });
-                  }
-                }
-              );
-            }
-          });
-        }
-      });
-    }
-  });
+  // con.connect((err) => {
+  //   if (err) {
+  //     throw err;
+  //   } else {
+  //     console.log("DB connectée");
+  //     con.query("CREATE DATABASE IF NOT EXISTS db", (err) => {
+  //       if (err) {
+  //         throw err;
+  //       } else {
+  //         con.query("USE db;", (err) => {
+  //           if (err) {
+  //             throw err;
+  //           } else {
+  //             con.query(
+  //               "CREATE TABLE IF NOT EXISTS personnage (id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,name varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,photo varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,characteristics text COLLATE utf8mb4_unicode_ci);",
+  //               (err) => {
+  //                 if (err) {
+  //                   throw err;
+  //                 } else {
+  //                   console.log("Base de données initialisée");
+  //                   con.end((err) => {
+  //                     if (err) throw err;
+  //                   });
+  //                 }
+  //               }
+  //             );
+  //           }
+  //         });
+  //       }
+  //     });
+  //   }
+  // });
 });
 
 // For testing
